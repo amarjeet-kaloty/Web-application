79df19345db3cbc88451959d04d6002d2cc43ba1
Amarjeet <60272668+amarjeet-kaloty@users.noreply.github.com>
2020-03-09 22:21:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+getComments();
+
+function getComments(){
+    $.get("/comments", (data) =>{
+        if(!data){
+            console.log("No data");
+        }
+        console.log("Data received");
+        for(var i = 0; i < data.length; i++){
+            console.log(data[i].name);
+        }
+        showComments(data);
+    });
+}
+
+function showComments(comments){
+    var commentsSection = document.getElementById("suggestions");
+    for(var i = 0; i < comments.length; i++){
+        var section = document.createElement("section");
+        section.className += 'suggestion';
+        var heading = document.createElement('h3');
+        heading.innerHTML = comments[i].name;
+        var comment = document.createElement('p');
+        comment.innerHTML = comments[i].comment;
+        section.appendChild(heading);
+        section.appendChild(comment);
+        commentsSection.appendChild(section);
+    }
+}
